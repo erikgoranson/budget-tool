ab7b42f35a9574b00e14770b95006466da12a05a
Erik Goranson <esgoranson@gmail.com>
2024-09-03 00:46:11 -0600

    .

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -1,11 +1,9 @@
 import type { Budget } from "./Budget";
 
-//parent category: i.e., bills, debt, etc
 export type Category = {
     id: string,
     name: string,
-    //amount: number,
-    description: string, //needs to be optional somehow 
-    hasDueDates: boolean, //whether a Due column is added (which denotes the day of any given month wherestuff is due)
+    description: string, 
+    hasDueDates: boolean, 
     budgets: Array<Budget>,
 };
