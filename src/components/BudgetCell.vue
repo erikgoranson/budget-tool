5598c25747ec7f194cf5154c38ae1d93ed9fd6fc
erikgoranson <esgoranson@gmail.com>
2025-03-13 00:03:04 -0600

    tidying

--- src/components/BudgetCell.vue
+++ src/components/BudgetCell.vue
@@ -1,37 +1,16 @@
 <script setup lang="ts">
-
-//maybe rename this BudgetCell
-
-
-//REORGANIZE THESE
-
-//types
-//libraries
-//helpers +components
-//refs
-//consts
-//methods
-
 import type { Budget, Category } from '../types/';
-import { h, ref, computed, nextTick, watch, toRef, toRefs, reactive } from 'vue';
-import { storeToRefs } from 'pinia';
-import { valueUpdater } from '../lib/utils'; 
-import currencyFormatter from '../helpers/numberFormat'; 
-import { useForm, useField } from 'vee-validate';
-import { useTransactionStore } from '@/stores/transaction';
-import { useMediaQuery } from '@vueuse/core';
-import { ArrowUpDown, } from 'lucide-vue-next';
+import type { Cell } from '@tanstack/vue-table';
+import { ref, nextTick, computed } from 'vue';
+import { useForm } from 'vee-validate';
+import { FlexRender } from '@tanstack/vue-table';
 import { useCategoryStore } from '@/stores/category';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from '@/components/ui/table';
+import { Input } from '@/components/ui/input';
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from '@/components/ui/form';
-import type { Cell, ColumnDef,ColumnFiltersState, GlobalFilterTableState, SortingState, VisibilityState, } from '@tanstack/vue-table';
-import { FlexRender, getCoreRowModel, getExpandedRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useVueTable, } from '@tanstack/vue-table';
 
 const props = defineProps({
-    categoryId : {
-        type: String,
+    category : {
+        type: Object as () => Category,
         required: true
     },
     cell : {
@@ -45,40 +24,21 @@ const props = defineProps({
 });
 
 const categoryStore = useCategoryStore();
-const transactionStore = useTransactionStore();
 
 const editField = ref('');
 const modifiedBudget = ref<Budget>({} as Budget);
-const divs = ref([]); //to be used for autofocusing
-
 let inputs = ref<HTMLInputElement[]>([]);
-
 
 const { handleSubmit, errors, resetForm } = useForm({});
 
 const focusInput = async (key: string, index: number) => {
-    console.log('key is: ', key)
-    //console.log('props')
     editField.value = key;
-
-    //console.log('index is', index);
-    //console.log('new refs thing is: ', inputs.value)
-    //console.log('test', inputs.value[index])
-    //console.log('childrne are', inputs.value[index].children)
-
-
-    //console.log('ref is:', inputs.value[index]);
-    //console.log('childrne are', inputs.value[index].children)
-
     await nextTick();
     inputs.value[0].focus(); 
-    //this doesnt work becasue the index of inputs.value increases with every row of v-for
-    //A simpler way to do this is to use a dynamic ref name. We still need i and index to find the relevant element but there's no calculation required.
-    //https://stackoverflow.com/questions/58704223/how-to-access-an-elements-ref-index-within-the-nested-v-for-loop-in-vue-js
 };
 
 const onSubmit = handleSubmit((values, actions) => {
-    console.log(JSON.stringify(values, null, 2)) //debug
+    console.log(JSON.stringify(values, null, 2)); //debug
     const updatedBudget = <Budget>{
         id: modifiedBudget.value.id,
         name: values.name ? values.name : modifiedBudget.value.name,
@@ -87,7 +47,7 @@ const onSubmit = handleSubmit((values, actions) => {
     };
 
     const valuesMatch = JSON.stringify(updatedBudget) == JSON.stringify(modifiedBudget.value);
-    if (!valuesMatch) categoryStore.updateBudget(props.categoryId, updatedBudget);
+    if (!valuesMatch) categoryStore.updateBudget(props.category.id, updatedBudget);
     focusInput('', 0);
 });
 
@@ -95,7 +55,6 @@ const modifyBudget = (budget: Budget) => {
     modifiedBudget.value = budget;
     onSubmit();
 };
-
 </script>
 
 <template>
