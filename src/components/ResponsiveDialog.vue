e2628627965ff10591d9070f2ebfe5579f897dae
erikgoranson <esgoranson@gmail.com>
2025-04-11 12:51:40 -0600

    cleanup - fixing a warning from vue

--- src/components/ResponsiveDialog.vue
+++ src/components/ResponsiveDialog.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, defineEmits, computed, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { createReusableTemplate, useMediaQuery } from '@vueuse/core';
 
 import {
