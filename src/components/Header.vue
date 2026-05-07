21e9be241ce91680976c71ed5cbb3b9acb0bd9da
erikgoranson <esgoranson@gmail.com>
2026-05-07 01:35:07 -0600

    fix: split sidebar hamburger into its own component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import { AlignJustify } from 'lucide-vue-next';
-import { useSidebarStore } from '@/stores/sidebar';
 import BudgetDashboard from "./budgetRow/BudgetDashboard.vue";
-
-const sidebarStore = useSidebarStore();
 </script>
 
 <template>
     <header class="flex items-center justify-center px-4 py-4 bg-white border-b-4 border-indigo-600">
-        <div class="flex justify-start">
-            <button class="text-gray-500 focus:outline-none lg:hidden" @click="sidebarStore.isOpen = true">
-                <AlignJustify class="h-6 w-6" />
-            </button>
-        </div>
         <BudgetDashboard />
     </header>
 </template>
---
+++ src/components/SidebarToggle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { MenuIcon } from 'lucide-vue-next';
+import { useSidebarStore } from '@/stores/sidebar';
+const sidebarStore = useSidebarStore();
+</script>
+
+<template>
+    <button class="text-gray-500 focus:outline-none lg:hidden" @click="sidebarStore.isOpen = true">
+        <MenuIcon class="font-medium text-black-700 h-10 w-10"/>
+    </button>
+</template>
