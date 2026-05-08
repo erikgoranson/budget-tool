6e0927d2c835f49a5df5700728aa2c09165a566c
erikgoranson <esgoranson@gmail.com>
2026-05-08 15:04:57 -0600

    feat: add nav option to header for mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import { useRoute , useRouter } from 'vue-router';
+import { ChevronLeft } from 'lucide-vue-next';
+
 const route = useRoute();
+const router = useRouter();
 </script>
 
 <template>
-    <header class="flex items-center justify-center px-4 py-4 bg-white border-b-4 border-indigo-600 header pt-[env(safe-area-inset-top)]" >
-        <h1 class="text-4xl font-medium uppercase text-gray-900 mt-2">
+    <header class="flex items-center justify-center w-full h-16 px-4 bg-white border-b-4 border-indigo-600 relative pt-[env(safe-area-inset-top)]">
+        <div v-if="route.name !== 'home'" class="absolute left-4 p-2 rounded-full hover:bg-gray-100">
+            <ChevronLeft class="h-8 w-8 lg:hidden" @click="router.back()"/>
+        </div>
+
+        <h1 class="text-lg lg:text-4xl font-semibold text-gray-900 uppercase">
             {{ route.meta.displayName }}
         </h1>
     </header>
