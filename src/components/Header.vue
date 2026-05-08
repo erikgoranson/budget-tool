aa9cdda2bb5a287cc6cc248b766ee3f1021ed661
erikgoranson <esgoranson@gmail.com>
2026-05-08 16:22:37 -0600

    fix: header spacing issue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,12 +7,12 @@ const router = useRouter();
 </script>
 
 <template>
-    <header class="flex items-center justify-center w-full h-16 px-4 bg-white border-b-4 border-indigo-600 relative pt-[env(safe-area-inset-top)]">
-        <div v-if="route.name !== 'home'" class="absolute left-4 p-2 rounded-full hover:bg-gray-100">
+    <header class="flex items-center justify-center w-full bg-indigo-200 border-indigo-600 pt-[env(safe-area-inset-top)] relative" >
+        <div v-if="route.name !== 'home'" class="absolute left-4 p-2">
             <ChevronLeft class="h-8 w-8 lg:hidden" @click="router.back()"/>
         </div>
 
-        <h1 class="text-lg lg:text-4xl font-semibold text-gray-900 uppercase">
+        <h1 class="text-lg lg:text-4xl font-semibold py-2 text-gray-900 uppercase">
             {{ route.meta.displayName }}
         </h1>
     </header>
