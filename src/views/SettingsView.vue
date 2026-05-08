b52559ffcf64ad70e40fe9a37b2ca7d20b431457
erikgoranson <esgoranson@gmail.com>
2026-05-08 15:00:43 -0600

    style: rework settings page with shadcn item

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,18 +1,34 @@
 <script setup lang="ts">
-import { Separator } from '@/components/ui/separator';
 import SaveDataButton from '@/components/SaveDataButton.vue';
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemTitle,
+  ItemActions,
+} from '@/components/ui/item';
+import { ChevronRightIcon } from 'lucide-vue-next';
+
 </script>
 
 <template>
-<div>
-    <div class="flex items-center justify-between">
-        <div>Export All Data</div>
-        <SaveDataButton/>
+    <div class="flex flex-col gap-6">
+        <Item variant="outline" class="item">
+            <ItemContent>
+                <ItemTitle>Export All Data</ItemTitle>
+                <ItemDescription>
+                    Download all user data to a .json file.
+                </ItemDescription>
+            </ItemContent>
+            <ItemActions>
+                <SaveDataButton/>
+            </ItemActions>
+        </Item>
     </div>
-
-    <Separator class="my-4 bg-gray-400" />
-  </div>
 </template>
 
 <style scoped>
+.item {
+  @apply !border-gray-400;
+}
 </style>
