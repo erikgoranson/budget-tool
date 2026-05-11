3840e4a5acde6e0d2d89f99240d2573d7af718a7
erikgoranson <esgoranson@gmail.com>
2026-05-11 02:04:34 -0600

    style: handle ios notch for sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -29,7 +29,7 @@ const inactiveClass = ref(
         
         <div
             :class="sidebarStore.isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
-            class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-blue-400 lg:translate-x-0 lg:static lg:inset-0"
+            class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-blue-400 lg:translate-x-0 lg:static lg:inset-0 pt-[env(safe-area-inset-top)]"
         >
             <div class="flex items-center justify-center mt-8">
                 <div class="flex items-center">
