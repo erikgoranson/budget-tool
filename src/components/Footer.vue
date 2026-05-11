693751524dccf1761a3bdcc6382cd88985a52eda
erikgoranson <esgoranson@gmail.com>
2026-05-11 11:59:41 -0600

    style: fix footer positioning on ios

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,9 +4,9 @@ import SidebarToggle from './SidebarToggle.vue';
 </script>
 
 <template>
-    <footer class="flex items-center justify-center px-4 py-4 bg-indigo-200 border-t border-gray-200">
+    <footer class="flex items-center justify-center px-4 py-4 bg-indigo-200 border-t border-gray-200 pt-[env(keyboard-inset-height)]">
         <div class="flex-1 items-center justify-center mx-6">
-            <div class="flex ml-auto items-center justify-center flex space-x-6">
+            <div class="flex ml-auto items-center justify-center flex space-x-6 mt-4">
                 <CreateTransactionDialog />
                 <SidebarToggle />
             </div>
