a85d1b35e7a0c645d65efbf428f6637fbd1b7ae3
erikgoranson <esgoranson@gmail.com>
2026-05-12 02:23:19 -0600

    fix: virtual keyboard viewport problems

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -9,7 +9,7 @@ import Footer from '../components/Footer.vue';
     <Sidebar />
     <div class="flex-1 flex flex-col overflow-hidden">
       <Header />
-      <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+      <main class="flex-1 h-screen overflow-x-hidden overflow-y-auto bg-gray-200 relative ">
         <div class="container mx-auto px-6 py-8">
           <slot />
         </div>
