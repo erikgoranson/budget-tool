b8514921dbe9625d57b91c807209a0752a5515fb
erikgoranson <esgoranson@gmail.com>
2026-05-14 17:19:31 -0600

    feat: option to delete localstorage

---
+++ src/components/EraseLocalStorage.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useBudgetDataStore } from '@/stores/budgetData';
+import { AlertDialog, AlertDialogAction,AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter,AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
+
+const dataStore = useBudgetDataStore();
+const deleteData = () => {
+    dataStore.truncateBudgetData();
+}
+</script>
+
+<template>
+    <AlertDialog>
+        <AlertDialogTrigger as-child>
+            <div @click="deleteData">
+                <slot/>
+            </div>
+        </AlertDialogTrigger>
+    <AlertDialogContent>
+        <AlertDialogHeader>
+            <AlertDialogTitle>
+                Are you sure?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+                This action cannot be undone. This action will permanently delete all the data you have entered so far and it cannot be recovered unless you have already downloaded a copy. 
+            </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+            <AlertDialogAction class="bg-destructive text-destructive-foreground hover:bg-destructive/90">
+                Delete
+            </AlertDialogAction>
+            <AlertDialogCancel>
+                Cancel
+            </AlertDialogCancel>
+        </AlertDialogFooter>
+    </AlertDialogContent>
+    </AlertDialog>
+</template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,47 +1,67 @@
 <script setup lang="ts">
+import { h } from 'vue';
+import { RouterLink } from 'vue-router';
+import Button from '@/components/ui/button/Button.vue';
+import { Item, ItemContent, ItemDescription, ItemTitle, ItemActions } from '@/components/ui/item';
+import { ChevronRightIcon, Shredder, ShredderIcon, Download } from 'lucide-vue-next';
+
 import SaveDataButton from '@/components/SaveDataButton.vue';
-import {
-  Item,
-  ItemContent,
-  ItemDescription,
-  ItemTitle,
-  ItemActions,
-} from '@/components/ui/item';
-import { ChevronRightIcon } from 'lucide-vue-next';
+import EraseLocalStorage from '@/components/EraseLocalStorage.vue'; //need to rename
+
+const importButton = () => {
+    return h(
+      RouterLink,
+      { to: '/import' },
+      {
+        default: () => h(ChevronRightIcon, { class: 'size-8' })
+      }
+    );
+};
+
+const deleteButton = () => {
+    return h(EraseLocalStorage, null, {
+      default: () => [
+        h(Button, null, {
+          default: () => [
+            h(ShredderIcon),
+            'Delete'
+          ]
+        })
+      ]
+    });
+};
+
+const settingsOptions = [
+    {
+        title: 'Export Budget Data',
+        description: 'Download data to a .json file.',
+        action: SaveDataButton,
+    },
+    {
+        title: 'Import Budget Data',
+        description: 'Import data using a previously downloaded .json file',
+        action: importButton,
+    },
+    {
+        title: 'Delete Budget Data',
+        description: 'Delete all budget data that has been saved to local storage',
+        action: deleteButton,
+    }
+];
 </script>
 
 <template>
     <div class="flex flex-col gap-6">
-        <Item variant="outline" class="item">
+        <Item v-for="option in settingsOptions" variant="outline" class="!border-gray-400">
             <ItemContent>
-                <ItemTitle>Export Budget Data</ItemTitle>
+                <ItemTitle>{{option.title}}</ItemTitle>
                 <ItemDescription>
-                    Download data to a .json file.
+                    {{ option.description}}
                 </ItemDescription>
             </ItemContent>
             <ItemActions>
-                <SaveDataButton/>
-            </ItemActions>
-        </Item>
-
-        <Item variant="outline" class="item">
-            <ItemContent>
-                <ItemTitle>Import Budget Data</ItemTitle>
-                <ItemDescription>
-                    Import data using a previously downloaded .json file
-                </ItemDescription>
-            </ItemContent>
-            <ItemActions>
-                <router-link to="/import">
-                    <ChevronRightIcon class="size-6" />
-                </router-link>
+                <component :is="option.action" />
             </ItemActions>
         </Item>
     </div>
 </template>
-
-<style scoped>
-.item {
-  @apply !border-gray-400;
-}
-</style>
