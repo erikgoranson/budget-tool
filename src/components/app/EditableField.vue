e988a8072cb609f8378642921ec51ff464916507
erikgoranson <esgoranson@gmail.com>
2026-05-20 02:35:15 -0600

    feat: new field for inline editing regardless of type

---
+++ src/components/app/EditableField.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts" generic="T">
+import { ref } from 'vue';
+import { onClickOutside } from '@vueuse/core';
+
+const props = defineProps<{
+  label?: string
+  type?: 'text' | 'number' | 'email' | 'date'
+}>();
+
+const model = defineModel<T>({ required: true });
+
+const isEditing = ref<boolean>(false);
+const tempValue = ref<T>(model.value);
+
+const target = ref<HTMLElement | null>(null);
+
+const startEditing = () => {
+  tempValue.value = model.value;
+  isEditing.value = true;
+}
+
+const save = () => {
+  model.value = tempValue.value;
+  isEditing.value = false;
+}
+
+const cancel = () => {
+  isEditing.value = false;
+}
+
+onClickOutside(target, () => {
+  if (isEditing.value) {
+    save();
+  }
+})
+</script>
+
+<template>
+    <div>
+        <div v-if="isEditing" >
+            <label v-if="props.label">{{ props.label }}</label>
+            <input 
+                ref="target"
+                v-model="tempValue" 
+                :type="props.type || 'text'" 
+                autofocus
+                @keydown.enter="save"
+                @keydown.escape="cancel"
+            />
+        </div>
+        
+        <div v-else  @click="startEditing">
+            <span v-if="props.label" >{{ props.label }}:</span>
+            <span>{{ model }}</span>
+        </div>
+    </div>
+</template>
