cac52e30c20c70a8cc468271e8de75aab2031294
erikgoranson <esgoranson@gmail.com>
2026-05-21 16:45:42 -0600

    refactor: rework table using new components

--- src/components/transaction/TransactionTable.vue
+++ src/components/transaction/TransactionTable.vue
@@ -1,50 +1,78 @@
-<script setup lang="ts">
-import type {
-  ColumnDef,
-  ColumnFiltersState,
-  GlobalFilterTableState,
-  SortingState,
-  VisibilityState,
-} from '@tanstack/vue-table';
+<script setup lang="ts" >
+import type { ColumnDef, ColumnFiltersState, GlobalFilterTableState, SortingState, VisibilityState } from '@tanstack/vue-table';
 import type { Transaction } from '@/types';
-import { useMediaQuery } from '@vueuse/core';
-
+import { storeToRefs } from 'pinia'
 import { h, ref, computed } from 'vue';
-import { storeToRefs } from 'pinia';
-
+import { useMediaQuery } from '@vueuse/core';
 import { valueUpdater } from '@/lib/utils'; 
-import currencyFormatter from '@/helpers/numberFormat'; 
-import { transactionColumns } from './transactionColumns';
+import currencyFormatter from '@/helpers/numberFormat';
+import { Button } from '../ui/button';
+
+import { FilePenLine, ArrowUpDown } from 'lucide-vue-next';
+import { FlexRender, getCoreRowModel, getExpandedRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useVueTable } from '@tanstack/vue-table';
+import { useDynamicColumns } from '@/composables/useDynamicColumns';
+import TransactionActionsMenu from './TransactionActionsMenu.vue';
+import TransactionDetailCard from './TransactionDetailCard.vue';
+
 import { useTransactionStore } from '@/stores/transaction';
-
-import {
-  FlexRender,
-  getCoreRowModel,
-  getExpandedRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useVueTable,
-} from '@tanstack/vue-table'
-import { ChevronDown,} from 'lucide-vue-next';
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
-
 const transactionStore = useTransactionStore();
 const { transactionRows } = storeToRefs(transactionStore);
+
+const { createReadOnlyColumn, createSelectorColumn, createSortableHeader } = useDynamicColumns(transactionRows);
+const columns = [
+    createSelectorColumn(),
+    //mobile
+    createReadOnlyColumn('date', 'Date'),
+    createReadOnlyColumn('budgetCategoryName', 'Category'),
+    createReadOnlyColumn('note', 'Note'),
+    {
+        accessorKey: 'amount',
+        header: ({ column }) => createSortableHeader(column, 'Amount'),
+        cell: ({ row }) => {
+            const amount = Number.parseFloat(row.getValue('amount'))
+            const isIncome = row.original.income;
+            const formattedAmt = currencyFormatter.format(amount);
+            const flowStyle = isIncome ? 'text-green-500' : 'text-red-500';
+            return h('div', { class: `font-medium font-semibold ${flowStyle}` }, formattedAmt)
+        },
+    },
+    {
+        id: 'mobile',
+        header: ({ table }) => {
+            const dateColumn = table.getColumn('date');
+            return h(Button, { 
+                variant: 'ghost', 
+                onClick: () => { 
+                    if (dateColumn) {
+                        dateColumn.toggleSorting(dateColumn.getIsSorted() === 'asc');
+                    }
+                }}, 
+                () => [
+                    'Details', 
+                    h(ArrowUpDown, {  class: 'ml-2 h-4 w-4' })
+                ]
+            );
+        },
+        enableSorting: true,
+        cell: ({ row }) => {
+            return h(TransactionDetailCard, {
+                data: row.original,
+            })
+        },
+    },
+    {
+        id: 'actions',
+        header: () => ['', h(FilePenLine, { class: 'ml-2 h-4 w-4' })],
+        enableHiding: false,
+        cell: ({ row }) => {
+            return h(TransactionActionsMenu, {
+                transaction: row.original,
+            })
+        },
+    },
+];
+
+
 
 const sorting = ref<SortingState>([
   { id: 'date', desc: true }
@@ -53,21 +81,6 @@ const columnFilters = ref<ColumnFiltersState>([]);
 const rowSelection = ref({});
 const filter = ref<GlobalFilterTableState>();
 const isDesktop = useMediaQuery('(min-width: 768px)');
-
-const selectedTotalText = computed(() => {
-    const selectedRows = table.value.getFilteredSelectedRowModel().rows;
-    const rowAmounts = selectedRows.map(x => x.original.amount);
-    
-    if(selectedRows.length == 0){
-        return `${currencyFormatter.format(0)} total`;
-    } else {
-        const total = rowAmounts.reduce((a, b) => a + b);
-        return `${currencyFormatter.format(total)} total.`;
-    }
-});
-
-const columns = (transactionColumns as Transaction[]).sort((a, b) => Date.parse(a.date as string) - Date.parse(b.date));
-
 const columnVisibility = computed<VisibilityState>(() => ({
     mobile: !isDesktop.value,
     date: isDesktop.value,
@@ -76,98 +89,30 @@ const columnVisibility = computed<VisibilityState>(() => ({
     amount: isDesktop.value,
 }));
 
-
-const table = computed(() => { 
-    return useVueTable({
-        data: transactionRows,
-        columns: columns,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        getFilteredRowModel: getFilteredRowModel(),
-        onSortingChange: updaterOrValue => valueUpdater(updaterOrValue, sorting),
-        onColumnFiltersChange: updaterOrValue => valueUpdater(updaterOrValue, columnFilters),
-        onColumnVisibilityChange: updaterOrValue => valueUpdater(updaterOrValue, columnVisibility),
-        onRowSelectionChange: updaterOrValue => valueUpdater(updaterOrValue, rowSelection),
-        state: {
-            get sorting() { return sorting.value },
-            get columnFilters() { return columnFilters.value },
-            get columnVisibility() { return columnVisibility.value },
-            get rowSelection() { return rowSelection.value },
-            get globalFilter() { return filter.value },
-        },
-    })
+const table = useVueTable({
+    get data() { return transactionRows.value },
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: updaterOrValue => valueUpdater(updaterOrValue, sorting),
+    onColumnFiltersChange: updaterOrValue => valueUpdater(updaterOrValue, columnFilters),
+    onColumnVisibilityChange: updaterOrValue => valueUpdater(updaterOrValue, columnVisibility),
+    onRowSelectionChange: updaterOrValue => valueUpdater(updaterOrValue, rowSelection),
+    state: {
+        get sorting() { return sorting.value },
+        get columnFilters() { return columnFilters.value },
+        get columnVisibility() { return columnVisibility.value },
+        get rowSelection() { return rowSelection.value },
+        get globalFilter() { return filter.value },
+    },
 });
+
+import BaseTransactionTable from './BaseTransactionTable.vue';
+
 </script>
 
 <template>
-
-    <div class="flex items-center py-4">
-        <input
-            type="text"
-            class="border border-gray-400 rounded px-2 py-2"
-            placeholder="Filter by any column ... "
-            v-model="filter"
-        />
-        <DropdownMenu >
-            <DropdownMenuTrigger as-child>
-                <Button variant="outline" class="ml-auto">
-                    Columns <ChevronDown class="ml-2 h-4 w-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuCheckboxItem v-for="column in table.getAllColumns().filter((column) => column.getCanHide())" :key="column.id" class="capitalize" :checked="column.getIsVisible()" @update:checked="(value) => { 
-                    column.toggleVisibility(!!value)
-                }">
-                    {{ column.id }}
-                </DropdownMenuCheckboxItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    </div>
-
-    <div class="rounded-md border">
-        
-        <Table class="bg-card rounded-md">
-            <TableHeader>
-                <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id" >
-                    <TableHead v-for="header in headerGroup.headers" :key="header.id">
-                        <FlexRender v-if="!header.isPlaceholder" :render="header.column.columnDef.header" :props="header.getContext()" />
-                    </TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <template v-if="table.getRowModel().rows?.length">
-                    <template v-for="row in table.getRowModel().rows" :key="row.id">
-                        <TableRow :data-state="row.getIsSelected() && 'selected'" class="border-b border-gray-400">
-                            <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-                                <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
-                            </TableCell>
-                        </TableRow>
-                    </template>
-                </template>
-
-                <TableRow v-else>
-                    <TableCell :colspan="columns.length" class="h-24 text-center">
-                        No results.
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
-    </div>
-
-    <div class="flex items-center justify-end space-x-2 py-4">
-        <div class="flex-1 text-muted-foreground">
-            <div>{{ table.getFilteredSelectedRowModel().rows.length }} of {{ table.getFilteredRowModel().rows.length }} row(s) selected.</div>
-            <div>{{ selectedTotalText }}</div>
-        </div>
-        <div class="space-x-2">
-            <Button variant="outline" size="sm" :disabled="!table.getCanPreviousPage()" @click="table.previousPage()">
-                Previous
-            </Button>
-            <Button variant="outline" size="sm" :disabled="!table.getCanNextPage()" @click="table.nextPage()">
-                Next
-            </Button>
-        </div>
-    </div>
-
+    <BaseTransactionTable v-model:table="table" v-model:filter="filter"/>
 </template>
