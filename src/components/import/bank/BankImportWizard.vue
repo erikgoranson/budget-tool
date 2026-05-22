9bc841436490aebbd4cf20e64c5be3baaf9aca09
erikgoranson <esgoranson@gmail.com>
2026-05-22 01:40:29 -0600

    feat: configure step navigation

--- src/components/import/bank/BankImportWizard.vue
+++ src/components/import/bank/BankImportWizard.vue
@@ -3,22 +3,31 @@ import type { TransactionImport } from '@/types/TransactionImport';
 import { ref } from 'vue';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle, } from '@/components/ui/card';
-import CsvParser from './steps/CsvParser.vue';
+
+import CsvUploadForm from './steps/CsvUploadForm.vue';
+import ColumnMappingMenu from './steps/ColumnMappingMenu.vue';
 
 const importData = ref<TransactionImport>({} as TransactionImport);
 
 const currentStep = ref<number>(0); 
 const stepComplete = ref<boolean>(false);
-const steps = [];
+const steps = [
+  CsvUploadForm,
+  ColumnMappingMenu,
+];
 
 const setStep = (step: number) => {
   currentStep.value = step;
   stepComplete.value = false;
 };
-
 </script>
 
 <template>
+
+  <div class="my-10 h-20 overflow-y-scroll">
+    <pre>{{ JSON.stringify(importData, null, 2) }}</pre>
+  </div>
+
   <div>
     <Card class="border-gray-400 ">
       <CardHeader>
@@ -28,16 +37,26 @@ const setStep = (step: number) => {
       </CardHeader>
 
       <CardContent>
-
-        <CsvParser v-model="importData" v-model:stepComplete="stepComplete"/>
-
-        <!-- {{ importData.fileData }} -->
-
+        <component 
+            :is="steps[currentStep]" 
+            v-model="importData" 
+            v-model:stepComplete="stepComplete"
+          />
       </CardContent>
 
       <CardFooter class="mt-5 border-t pt-5 justify-between">
-        <Button @click="setStep(currentStep -1)">Prev</Button>
-        <Button @click="setStep(currentStep +1)">Next</Button>
+        <Button 
+          :disabled="currentStep == 0" 
+          @click="setStep(currentStep -1)"
+        >
+            Prev
+        </Button>
+        <Button 
+          :disabled="!stepComplete || currentStep+1 >= steps.length" 
+          @click="setStep(currentStep +1)"
+        >
+          Next
+        </Button>
       </CardFooter>
     </Card>
   </div>
