0f69ee135c274546fbf9d326ff0922c94cd21c10
erikgoranson <esgoranson@gmail.com>
2026-05-22 00:14:48 -0600

    feat: initial setup for multiple import steps

--- src/components/import/bank/BankImportWizard.vue
+++ src/components/import/bank/BankImportWizard.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle, } from '@/components/ui/card';
 import CsvParser from './steps/CsvParser.vue';
+
+const currentStep = ref<number>(0); 
+const stepComplete = ref<boolean>(false);
+const steps = [];
+
+const setStep = (step: number) => {
+  currentStep.value = step;
+  stepComplete.value = false;
+};
+
 </script>
 
 <template>
@@ -8,7 +20,7 @@ import CsvParser from './steps/CsvParser.vue';
     <Card class="border-gray-400 ">
       <CardHeader>
         <CardTitle>
-          Bank File Import
+          Bank File Import: Step {{currentStep + 1}} of {{steps.length}}
         </CardTitle>
       </CardHeader>
 
@@ -18,7 +30,9 @@ import CsvParser from './steps/CsvParser.vue';
 
       </CardContent>
 
-      <CardFooter>
+      <CardFooter class="mt-5 border-t pt-5 justify-between">
+        <Button @click="setStep(currentStep -1)">Prev</Button>
+        <Button @click="setStep(currentStep +1)">Next</Button>
       </CardFooter>
     </Card>
   </div>
